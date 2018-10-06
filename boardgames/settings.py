b005538496f819c13558ca0f04f8cd443219b22f
Jonathan Hernandez <jbhernan@gmail.com>
2018-10-06 10:31:10 -0700

    database for heroku

--- boardgames/settings.py
+++ boardgames/settings.py
@@ -73,18 +73,18 @@ WSGI_APPLICATION = 'boardgames.wsgi.application'
 
 # Database
 # https://docs.djangoproject.com/en/2.1/ref/settings/#databases
-DATABASES = {
-    'default': dj_database_url.config(
-        default=config('DATABASE_URL')
-    )
-}
 #DATABASES = {
-#    'default': {
-#        'ENGINE': 'django.db.backends.sqlite3',
-#        'NAME': os.path.join(BASE_DIR, 'db.sqlite3'),
-#    }
+#    'default': dj_database_url.config(
+#        default=config('DATABASE_URL')
+#    )
 #}
-
+DATABASES = {
+    'default': {
+        'ENGINE': 'django.db.backends.sqlite3',
+        'NAME': os.path.join(BASE_DIR, 'db.sqlite3'),
+    }
+}
+DATABASES['default'] = dj_database_url.config(conn_max_age=600, ssl_require=True)
 
 # Password validation
 # https://docs.djangoproject.com/en/2.1/ref/settings/#auth-password-validators
