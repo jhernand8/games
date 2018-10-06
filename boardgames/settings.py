cdac7803d67fff0b6b09e8669246cc484c2170a4
Jonathan Hernandez <jbhernan@gmail.com>
2018-10-06 10:20:00 -0700

    db

--- boardgames/settings.py
+++ boardgames/settings.py
@@ -73,13 +73,17 @@ WSGI_APPLICATION = 'boardgames.wsgi.application'
 
 # Database
 # https://docs.djangoproject.com/en/2.1/ref/settings/#databases
-
 DATABASES = {
-    'default': {
-        'ENGINE': 'django.db.backends.sqlite3',
-        'NAME': os.path.join(BASE_DIR, 'db.sqlite3'),
-    }
+    'default': dj_database_url.config(
+        default=config('DATABASE_URL')
+    )
 }
+#DATABASES = {
+#    'default': {
+#        'ENGINE': 'django.db.backends.sqlite3',
+#        'NAME': os.path.join(BASE_DIR, 'db.sqlite3'),
+#    }
+#}
 
 
 # Password validation
