225bdbb2d29d00d9e14a5d98f0308dd59c0ff0e7
Jonathan Hernandez <jbhernan@gmail.com>
2023-12-18 15:19:31 -0600

    models and migrations

--- boardgames/models.py
+++ boardgames/models.py
@@ -19,3 +19,21 @@ class GamePlay(models.Model):
   bggId = models.IntegerField()
   date = models.DateField()
   notes = models.TextField(null = True)
+
+#
+class HNStory(models.Model):
+  storyJSON = models.TextField()
+  hnUserId = models.IntegerField()
+  hnStoryId = models.IntegerField(unique = True, primary_key = True)
+
+# object for storing the list of top HN story ids as of a given time.
+class TopStoryIdsByTime(models.Model):
+  storyIds = models.TextField()
+  topTime = models.DateTimeField(auto_now = True, unique = True, primary_key = True)
+
+# Object for storing a top story from HN.
+class HNTopStory(models.Model):
+  hnStoryId = models.IntegerField(unique = True, primary_key = True)
+  date = models.DateField(auto_now = True)
+  story = models.TextField()
+  marked_deleted = models.BooleanField(default = False)
