5dea6c5f005ea39df2a4a85bd95923aa39cab3ae
Jonathan Hernandez <jbhernan@gmail.com>
2023-12-18 20:05:42 -0600

    Added models for food stuff

--- boardgames/models.py
+++ boardgames/models.py
@@ -37,3 +37,32 @@ class HNTopStory(models.Model):
   date = models.DateField(auto_now = True)
   story = models.TextField()
   marked_deleted = models.BooleanField(default = False)
+
+
+# Object for a particular meal - where, when, meal, what had, who else was there, etc.
+class Meal(models.Model):
+  date = models.DateField()
+  meal = models.TextField()
+  venue = models.TextField()
+  yelpUrl = models.TextField()
+  food = models.TextField()
+  attendee = models.TextField()
+  meal_id = models.AutoField(primary_key = True)
+
+# Object for tracking restaurants to try
+class RestaurantToTry(models.Model):
+  yelpUrl = models.TextField()
+  venue = models.TextField()
+  notes = models.TextField()
+  area = models.TextField()
+  food = models.TextField()
+
+
+# Object for other things tracking.
+class Other(models.Model):
+  date = models.DateField()
+  type = models.TextField()
+  desc = models.TextField()
+  attendee = models.TextField()
+  other_id = models.AutoField(primary_key = True)
+
